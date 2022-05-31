18cc4cd631457f39e84339cb26eb6b9b84f2df1b
timvanderstam <timvanderstam@hotmail.com>
2022-05-31 13:18:27 +0200

    preselll.php functioneel gemaakt en een beetje mooier.

--- rent-a-car/preselll.php
+++ rent-a-car/preselll.php
@@ -1,8 +1,69 @@
 <?php
 include 'core/init.php';
 include 'includes/overall/header.php';
-$car_id = $_POST['car_id']?>
-<h1>enter you data to proceed</h1>
+if (empty($_POST['car_id']) === false) {
+    $_SESSION['car_id'] = $_POST['car_id'];
+} else if (empty($_POST) === false) {
+    $required_fields = array('naam', 'email', 'ophalen_op_date', 'ophalen_op_time', 'terug_brengen_op_date', 'terug_brengen_op_time');
+    foreach ($_POST as $key => $value) {
+        if (empty($value) && in_array($key, $required_fields) === true) {
+            $errors[] = 'Fields marked with an * are required';
+            break 1;
+        }
+    }
+
+    if (empty($errors) === true) {
+        if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) === false) {
+            $errors[] = 'a valid email adres is required';
+        }
+        if (email_exists($_POST['email']) === true) {
+            $errors[] = 'sorry, the email \'' . $_POST['email'] . '\' is already in use';
+        }
+    }
+}
+
+$car_id = $_SESSION['car_id'];
+
+
+?>
+<h1>persoonlijke informatie invullen</h1>
+<?php if (empty($errors) === false) {
+    echo output_errors($errors);
+} ?>
+<div id="presellcontainer">
+    <?php echo get_selected_car($car_id); ?>
+    <form action="" method="post">
+        <ul>
+            <li>
+                voornaam*: <br>
+                <input type="text" name="naam">
+            </li>
+            <li>
+                tussenvoegsel: <br>
+                <input type="text" name="achternaam">
+            </li>
+            <li>
+                achternaam: <br>
+                <input type="text" name="achternaam">
+            </li>
+            <li>
+                Email*: <br>
+                <input type="text" name="email">
+            </li>
+            <li>
+                Ophalen op*: <br>
+                <input type="date" name="ophalen_op_date">
+                <input type="time" name="ophalen_op_time">
+            </li>
+            <li>
+                Terug brengen op*: <br>
+                <input type="date" name="terug_brengen_op_date">
+                <input type="time" name="terug_brengen_op_time">
+            </li>
+            <input type="submit" class="button" value="Naar factuur">
+        </ul>
+    </form>
+</div>
 <?php
-echo get_selected_car($car_id);
-include 'includes/overall/footer.php'; ?>
+include 'includes/overall/footer.php';
+?>
